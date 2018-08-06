61285f1d3575243a0d27822b74198072edc5ff44
Dyulin <40668726+Dyulin@users.noreply.github.com>
2018-08-07 00:21:44 +0800

    部分文件模块

--- back/lab/Application/Admin/Controller/FileController.class.php
+++ back/lab/Application/Admin/Controller/FileController.class.php
@@ -3,22 +3,21 @@ namespace Admin\Controller;
 use Admin\Common\ApiController;
 class FileController extends ApiController
 {
-public function upload(){
+      spublic function upload(){
   #foreach ($variable as $key => $value) {
     # code...
   #}
       $_account=session('account');
-      echo $_account;
       $upload = new \Think\Upload();// 实例化上传类
       $upload->maxSize   =    10*1024*1024 ;// 设置附件上传大小
       $upload->exts      =     array('doc', 'docx', 'xls', 'txt','png','jpg');// 设置附件上传类型
       $upload->rootPath  =     './Public/'; // 设置附件上传根目录  
-      $upload->savePath  =     'test1/'.$_account.'/'; // 设置附件上传（子）目录 后续可利用个人session来创建个人目录
+      $upload->savePath  =     'test1/'; // 设置附件上传（子）目录 后续可利用个人session来创建个人目录
       $upload->autoSub   =  false; //不生成时间目录
       $upload->saveName  = '';
       // 上传文件
       $info  =   $upload->upload();
-      $path  =   'F:/XAMPP/htdocs/dyl/lab/Public/'.$info['file']['savepath'].$info['file']['savename'];
+      $path  =   'F:/XAMPP/htdocs/dyl/lab/Public/'.$info['file']['savepath'];
       $name=$info['file']['savename'];
       $time=date("Y/m/d"); 
       $file= D('File');
@@ -29,23 +28,71 @@ public function upload(){
         $this->apiReturn();
       }
     }  
-/*public function download(){
-  //path='F:\XAMPP\htdocs\dyl\lab\Public\test1\';
-  $http=new \Org\Net\Http();
-  $http->download($path);
-  }*/
-public function see(){
+/*public function see(){
   $filepath='./Public/' ;
   print_r(readDirectory($filepath));
-}
-
-/*public function uploadd(){
-  print_r($_FILES);
-  $filename=$_FILES['file']['name'];
-  $tmp_name=$_FILES['file']['tmp_name'];
-  move_uploaded_file($tmp_name, "./Public/test2/".$filename);
-
 }*/
-
+  public function seefile(){
+    $path="./Public/test1/";
+    $info=readDirectory($path);
+   //print_r($info);
+    if($info['file'])
+    {
+      $i=1;
+      foreach($info['file'] as $val)
+      {
+          $p=$path."/".$val;
+          $f[$i-1][id]=$i;
+          $f[$i-1][name]=$val;
+          $f[$i-1][size]=transByte(filesize($p));
+          $f[$i-1][time]=date("Y-m-d H:i:s",filectime($p));
+          $i++;
+      }
+      $this->apiReturn($f);
+   }
+  }
+  public function seedir(){
+    $path="./Public/test1/";
+    $info=readDirectory($path);
+   //print_r($info);
+    if($info['dir'])
+    {
+      $i=$i==null?1:$i;
+      foreach($info['dir'] as $val)
+      {
+          $p=$path."/".$val;
+          $dir[$i-1][id]=$i;
+          $dir[$i-1][name]=$val;
+          $dir[$i-1][size]=transByte(dirsize($p));
+          $dir[$i-1][time]=date("Y-m-d H:i:s",filectime($p));
+          $i++;
+      }
+      $this->apiReturn($dir);
+   }
+  }
+  public function download()
+  {
+    $filename=I('POST.filename');
+    $this->downFile($filename);
+    $this->apiReturn();
+  }
+  public function delfile()
+  {
+    $filename=I('POST.filename');
+    unlink($filename);
+    $this->apiReturn();
+  }
+    public function create_Folder()
+  {
+    $dirname=I('POST.dirname');
+    $this->createFolder($dirname);
+    $this->apiReturn();
+  }
+    public function del_folder()
+  {
+    $delpath=I('POST.delpath');
+    $this->delFolder($delpath);
+    $this->apiReturn();
+  }
 }  
 ?>
--- back/lab/Application/Common/Common/function.php
+++ back/lab/Application/Common/Common/function.php
@@ -15,3 +15,92 @@ function html_escape($var)
 	}
 	return $var;
 }
+
+function readDirectory($path)
+{
+//打开目录
+$handle = opendir($path);
+while(($item=readdir($handle))!==false)
+	{
+		if ($item!="."&&$item!="..") 
+		{
+			if(is_file($path."/".$item))
+			{
+				$arr['file'][]=$item;
+			}
+			if(is_dir($path."/".$item))
+			{
+			 	$arr['dir'][]=$item;
+			}
+		}
+	}
+closedir($handle); 
+return $arr;
+}
+function transByte($size) {
+	$arr = array ("B", "KB", "MB", "GB", "TB", "EB" );
+	$i = 0;
+	while ( $size >= 1024 ) {
+		$size /= 1024;
+		$i ++;
+	}
+	return round ( $size, 2 ) . $arr [$i];
+}
+function dirSize($path){
+	$sum=0;
+	global $sum;
+	$handle=opendir($path);
+	while(($item=readdir($handle))!==false){
+		if($item!="."&&$item!=".."){
+			if(is_file($path."/".$item)){
+				$sum+=filesize($path."/".$item);
+			}
+			if(is_dir($path."/".$item)){
+				$func=__FUNCTION__;
+				$func($path."/".$item);
+			}
+		}
+		
+	}
+	closedir($handle);
+	return $sum;
+}
+function downFile($filename){
+	header("content-disposition:attachment;filename=".basename($filename));
+	header("content-length:".filesize($filename));
+	readfile($filename);
+}
+function createFolder($dirname){
+	//检测文件夹名称的合法性
+	if(checkFilename(basename($dirname))){
+		//当前目录下是否存在同名文件夹名称
+		if(!file_exists($dirname)){
+			if(mkdir($dirname,0777,true)){
+				$mes="文件夹创建成功";
+			}else{
+				$mes="文件夹创建失败";
+			}
+		}else{
+			$mes="存在相同文件夹名称";
+		}
+	}else{
+		$mes="非法文件夹名称";
+	}
+	return $mes;
+}
+function delFolder($path){
+	$handle=opendir($path);
+	while(($item=readdir($handle))!==false){
+		if($item!="."&&$item!=".."){
+			if(is_file($path."/".$item)){
+				unlink($path."/".$item);
+			}
+			if(is_dir($path."/".$item)){
+				$func=__FUNCTION__;
+				$func($path."/".$item);
+			}
+		}
+	}
+	closedir($handle);
+	rmdir($path);
+}
